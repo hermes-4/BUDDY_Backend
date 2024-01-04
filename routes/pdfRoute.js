a554056166e0f276ce0009155855dcd44f1230ad
AbelTattah <abeltattah@gmail.com>
2024-01-04 14:20:45 +0000

    feat: Add Pdf download routes

--- routes/pdfRoute.js
+++ routes/pdfRoute.js
@@ -1,15 +1,43 @@
 const express =require('express');
 const path = require('path');
 const pdfRoute = express.Router();
-const filePath = path.join(__dirname, '../pdf', 'timetable.pdf');
+const filePath1 = path.join(__dirname, '../pdf', 'timetable.pdf');
+const filePath2 = path.join(__dirname, '../pdf', 'mathtimetable.pdf');
+const filePath3 = path.join(__dirname, '../pdf', 'statisticstimetable.pdf');
+const filePath4 = path.join(__dirname, '../pdf', 'academiccalendar.pdf');
+const filePath5 = path.join(__dirname, '../pdf', 'fees.pdf');
+const filePath6 = path.join(__dirname, '../pdf', 'examtimetable.pdf');
+
+
 
 pdfRoute.use((req,res,next)=>{
     next();
 });
 
-pdfRoute.get('/pdf1', (req, res) => {
-    res.download(filePath);
+pdfRoute.get('/cstimetable', (req, res) => {
+    res.download(filePath1);
 });
+
+pdfRoute.get('/mathtimetable', (req, res) => {
+    res.download(filePath2);
+});
+
+pdfRoute.get('/statisticstimetable', (req, res) => {
+    res.download(filePath3);
+});
+
+pdfRoute.get('/academiccalendar', (req, res) => {
+    res.download(filePath4);
+});
+
+pdfRoute.get('/fees', (req, res) => {
+    res.download(filePath5);
+});
+
+pdfRoute.get('/examtimetable', (req, res) => {
+    res.download(filePath6);
+});
+
 
 
 module.exports = pdfRoute;
