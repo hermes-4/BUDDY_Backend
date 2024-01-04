6586bd678ce35dcb38ca3dcbb7cbefa685db8b9c
AbelTattah <abeltattah@gmail.com>
2024-01-04 13:45:58 +0000

    fix: timetable route problem 1

--- routes/pdfRoute.js
+++ routes/pdfRoute.js
@@ -1,13 +1,15 @@
 const express =require('express');
 const path = require('path');
+const fs = require('../pdf');
 const pdfRoute = express.Router();
+const filePath = path.join(__dirname, 'pdf', 'timetable.pdf');
 
 pdfRoute.use((req,res,next)=>{
     next();
 });
 
 pdfRoute.get('/pdf1', (req, res) => {
-    res.download("../pdf/timetable.pdf");
+    res.download(filePath);
 });
 
 
